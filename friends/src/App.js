c3e147b89c15cb186475fc8c57627d9fe564170a
rndpatel@gmail.com <rndpatel@gmail.com>
2019-11-12 19:27:13 -0800

    it's working

--- friends/src/App.js
+++ friends/src/App.js
@@ -5,15 +5,16 @@ import Friends from "./components/Friends";
 
 import { Route, Link, Redirect } from 'react-router-dom';
 
+
 // With protected Routes, you'll be redirected to another page without the if statement being truthy
 const ProtectedRoute = ({component: Component, ...rest}) => {
-  return <Route {...rest} render={props => {
-    if(localStorage.getItem('token')) {
-      return <Component {...props}/>;
-    } else {
-      return <Redirect to="/login"/>
-    }
-  }}/>
+    return <Route {...rest} render={props => {
+        if(localStorage.getItem('token')) {
+            return <Component {...props}/>;
+        } else {
+            return <Redirect to="/login"/>
+        }
+    }}/>
 };
 
 const protectRoute = Component => props => {
@@ -24,15 +25,15 @@ const protectRoute = Component => props => {
     }
 };
 
-const ProtectedFriends = protectRoute(Friends);
+// const ProtectedFriends = protectRoute(Friends);
 
 function App() {
-  return (
-    <div className="App">
-      <Route path="/login" component={Login}/>
-      <ProtectedRoute path="/friends" component={Friends}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Route path="/login" component={Login}/>
+            <ProtectedRoute path="/friends" component={Friends}/>
+        </div>
+    );
 }
 
 export default App;
